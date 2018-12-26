de761f6cd5f67a8cd7cfe55b0f1775ab031f5f83
Dhananjay Singh <dhananjay1204@gmail.com>
2018-12-26 12:57:52 +0530

    Added travis integration

---
+++ docker_push.sh
@@ -0,0 +1,14 @@
+docker push dhananjay12/config-server:latest
+docker push dhananjay12/config-server:$SHA
+
+docker push dhananjay12/eureka-server:latest
+docker push dhananjay12/eureka-server:$SHA
+
+docker push dhananjay12/zuul-server:latest
+docker push dhananjay12/zuul-server:$SHA
+
+docker push dhananjay12/user-service:latest
+docker push dhananjay12/user-service:$SHA
+
+docker push dhananjay12/contact-us-service:latest
+docker push dhananjay12/contact-us-service:$SHA
