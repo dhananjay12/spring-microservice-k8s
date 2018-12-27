79599dfcd75d9a76680e62e145bbc7a081c447dc
Dhananjay Singh <dhananjay1204@gmail.com>
2018-12-27 12:54:32 +0530

    Travis deployment change

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ kubectl set image deployments/configserver configserver=dhananjay12/configserver
 kubectl set image deployments/contactus contactus=dhananjay12/contactus:$SHA
 kubectl set image deployments/eureka eureka=dhananjay12/eureka:$SHA
 kubectl set image deployments/user user=dhananjay12/user:$SHA
-kubectl set image deployments/zuul user=dhananjay12/zuul:$SHA
+kubectl set image deployments/zuul zuul=dhananjay12/zuul:$SHA
